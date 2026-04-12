7ac2b6c15d897328d520a6d53268011521d29df3
Amlibs <gam05oot@gmail.com>
2026-04-12 17:54:04 +0500

    minor changes

--- lab3/run.sh
+++ lab3/run.sh
@@ -2,4 +2,4 @@ rm -rf ./build
 mkdir build
 cmake . -B build/ -G "MinGW Makefiles" -DQt6_ROOT="D:\Qt\6.10.2\mingw_64"
 cmake --build build
-"D:\Qt\6.10.2\mingw_64\bin\windeployqt.exe" ".\build\qtlab3.exe"
+"D:\Qt\6.10.2\mingw_64\bin\windeployqt.exe" ".\build\main.exe"
